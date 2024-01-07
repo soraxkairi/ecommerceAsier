6361909f38cf66e3efafea21279df89637024b17
soraxkairi <asier.nogueira@ikasle.aeg.eus>
2024-01-07 01:47:18 +0100

    Finalizado ecommerce

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from './components/Navbar'
+import CartProvider from './components/Providers'
+import ShoppingCartModal from './components/ShoppingCartModal'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +18,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" >
+      <body className={inter.className}>
+        <CartProvider>
+          <Navbar />
+          <ShoppingCartModal/>
+          {children}
+        </CartProvider>
+      </body>
     </html>
   )
 }
