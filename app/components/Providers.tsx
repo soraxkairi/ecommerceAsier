1a0ad72d5de92461fd045d9fc9480d329bec3cee
Asier <101638375+soraxkairi@users.noreply.github.com>
2024-01-11 10:30:50 +0100

    Update Providers.tsx new url

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -10,8 +10,8 @@ export default function CartProvider({ children }: { children: ReactNode }) {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="https://ecommerceasier-kl5wrkg53-asier-nogueiras-projects.vercel.app/stripe/success"
-      cancelUrl="https://ecommerceasier-kl5wrkg53-asier-nogueiras-projects.vercel.app/stripe/error"
+      successUrl="https://ecommerceasier.vercel.app/stripe/success"
+      cancelUrl="https://ecommerceasier.vercel.app/stripe/error"
       currency="USD"
       billingAddressCollection={false}
       shouldPersist={true}
